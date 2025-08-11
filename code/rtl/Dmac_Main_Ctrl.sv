0774c535cb68930f6a5397a47bc6ff8a4fa9a27c
MuhammadMouzzam <mouzzamalimughal2005@gmail.com>
2025-08-11 20:42:19 +0500

    Adding Peripheral Config Implementation to Main Ctrl

--- code/rtl/Dmac_Main_Ctrl.sv
+++ code/rtl/Dmac_Main_Ctrl.sv
@@ -11,8 +11,8 @@
 // Authors: Muhammad Mouzzam and Danish Hassan 
 // Date: July 23rd, 2025
 
-typedef enum logic [1:0] {
-    IDLE                = 2'b00,
+typedef enum logic [2:0] {
+    IDLE                = 3'b000,
     BUS_REQD            = 3'b001,
     WAIT_FOR_DST        = 3'b010,
     WAIT_FOR_TRANS_SIZE = 3'b011,
@@ -22,24 +22,33 @@ typedef enum logic [1:0] {
     WAIT                = 3'b111
 } state_t;
 
+typedef enum logic [1:0] {
+    Idle    = 2'b00,
+    Busy    = 2'b01,
+    Non_Seq = 2'b10,
+    Seq     = 2'b11
+} HTrans_t;
+
 module Dmac_Main_Ctrl(
     input  logic clk,
     input  logic rst,
-    input  logic [1:0] DmacReq,
-    input  logic con_new_sel,
+    input  logic [1:0] DmacReq, DmacReq_Reg,
+    input  logic [1:0] con_new_sel,
     input  logic irq,
     input  logic Bus_Grant,
-    input  logic C_config,
+    input  logic C_config, HReady,
 
-    output logic con_sel,
+    output logic [1:0] con_sel,
     output logic Bus_Req,
+    output logic DmacReq_Reg_en, SAddr_Reg_en, DAddr_Reg_en, Trans_sz_Reg_en, Ctrl_Reg_en,
     output logic HReadyOut,
-    output logic [1:0] S_HResp,
+    output logic [1:0] S_HResp, addr_inc_sel,
     output logic Interrupt,
-    output logic con_en,
+    output logic con_en, config_write,
     output logic Channel_en_1, Channel_en_2,
     output logic hold,
-    output logic [1:0] ReqAck
+    output logic [1:0] ReqAck,
+    output HTrans_t config_HTrans
 );
 
     state_t current_state, next_state;
@@ -57,29 +66,111 @@ module Dmac_Main_Ctrl(
         Bus_Req      = 0;
         Interrupt    = 0;
         con_en       = 0;
-        con_sel      = 0;
+        con_sel      = 2'b00;
         Channel_en_1 = 0;
         Channel_en_2 = 0;
         HReadyOut    = 0;
+        SAddr_Reg_en = 0;
+        addr_inc_sel = 0;
+        config_write = 0;
+        DmacReq_Reg_en = 0;
+        DAddr_Reg_en = 0;
+        Trans_sz_Reg_en = 0;
+        Ctrl_Reg_en = 0;
         ReqAck       = 2'b00;
         S_HResp      = 2'b00;
+        config_HTrans = Idle;
         next_state   = current_state;  // Default next state
 
         case (current_state)
             IDLE: begin
-                
+                if(DmacReq != 2'b00) begin
+                    Bus_Req = 1;
+                    DmacReq_Reg_en = 1;
+                    SAddr_Reg_en = 1;
+                    next_state = BUS_REQD;
+                end
+            end
+
+            BUS_REQD: begin
+                if(Bus_Grant && HReady && DmacReq_Reg[1]) begin
+                    con_sel = 2'b10;
+                    config_write = 0;
+                    addr_inc_sel = 2'b00;
+                    config_HTrans = Non_Seq;
+                    ReqAck = 2'b10;
+                    next_state = WAIT_FOR_DST;
+                end else if(Bus_Grant && HReady && DmacReq_Reg == 2'b01) begin
+                    con_sel = 2'b10;
+                    config_write = 0;
+                    addr_inc_sel = 2'b00;
+                    config_HTrans = Non_Seq;
+                    ReqAck = 2'b01;
+                    next_state = WAIT_FOR_DST;
+                end
+            end
+
+            WAIT_FOR_DST: begin
+                if (!HReady) begin
+                    con_sel        = 2'b10;
+                    config_write   = 0;
+                    addr_inc_sel   = 2'b01;
+                    config_HTrans  = Busy;
+                end else if (Bus_Grant && HReady) begin
+                    con_sel        = 2'b10;
+                    config_write   = 0;
+                    addr_inc_sel   = 2'b01;
+                    config_HTrans  = Non_Seq;
+                    DAddr_Reg_en   = 1;
+                    next_state     = WAIT_FOR_TRANS_SIZE;
+                end
+            end
+
+            WAIT_FOR_TRANS_SIZE: begin
+                if (!HReady) begin
+                    con_sel        = 2'b10;
+                    config_write   = 0;
+                    addr_inc_sel   = 2'b10;
+                    config_HTrans  = Busy;
+                end else if (Bus_Grant && HReady) begin
+                    con_sel        = 2'b10;
+                    config_write   = 0;
+                    addr_inc_sel   = 2'b10;
+                    Trans_sz_Reg_en = 1;
+                    config_HTrans  = Non_Seq;
+                    next_state = WAIT_FOR_CTRL;
+                end
+            end
+
+            WAIT_FOR_CTRL: begin
+                con_sel        = 2'b10;
+                config_write   = 0;
+                addr_inc_sel   = 2'b10;
+
+                if (!HReady) begin
+                    config_HTrans = Busy;
+                end
+                else if (Bus_Grant && DmacReq_Reg[1]) begin
+                    Ctrl_Reg_en   = 1;
+                    config_HTrans = Idle;
+                    next_state    = MSB_REQ;
+                end
+                else if (Bus_Grant && (DmacReq_Reg == 2'b01)) begin
+                    Ctrl_Reg_en   = 1;
+                    config_HTrans = Idle;
+                    next_state    = LSB_REQ;
+                end
             end
 
             MSB_REQ: begin
                 if (!irq && ({C_config, Bus_Grant} == 2'b11)) begin
                     Channel_en_1 = 1;
                     con_en       = 0;
-                    con_sel      = 0;
-                    ReqAck       = 2'b10;
+                    con_sel      = 2'b00;
                     next_state   = WAIT;
                 end else if (irq && ({C_config, Bus_Grant} == 2'b11)) begin
                     Interrupt  = 1;
-                    con_sel = 1;
+                    con_sel = 2'b01;
                     Channel_en_1 = 1;
                     next_state = IDLE;
                 end else
@@ -90,8 +181,7 @@ module Dmac_Main_Ctrl(
                 if (!irq && ({C_config, Bus_Grant} == 2'b11)) begin
                     Channel_en_2 = 1;
                     con_en       = 1;
-                    con_sel      = 1;
-                    ReqAck       = 2'b01;
+                    con_sel      = 2'b01;
                     next_state   = WAIT;
                 end else if (irq && ({C_config, Bus_Grant} == 2'b11)) begin
                     Interrupt  = 1;
@@ -103,32 +193,32 @@ module Dmac_Main_Ctrl(
             end
 
             WAIT: begin
-                if (({Bus_Grant, con_new_sel} == 2'b00)) begin
+                if (({Bus_Grant, con_new_sel} == 3'b000)) begin
                     next_state = MSB_REQ;
                     Channel_en_1 = 0;
                     Bus_Req    = 1;
-                end else if (({Bus_Grant, con_new_sel} == 2'b01)) begin
+                end else if (({Bus_Grant, con_new_sel} == 3'b001)) begin
                     next_state = LSB_REQ;
                     Channel_en_2 = 0;
                     Bus_Req    = 1;
-                    con_sel    = 1;
+                    con_sel    = 2'b01;
                 end else
-                 if (!irq && !con_new_sel) begin
-                    con_sel    = 0;
+                 if (!irq && con_new_sel == 2'b00) begin
+                    con_sel    = 2'b00;
                     con_en     = 1;
                     Channel_en_1 = 1;
                     next_state = WAIT;
-                end else if (!irq && con_new_sel) begin
-                    con_sel    = 1;
+                end else if (!irq && con_new_sel == 2'b01) begin
+                    con_sel    = 2'b01;
                     con_en     = 1;
                     Channel_en_2 = 1;
                     next_state = WAIT;
-                end else if (irq && ({Bus_Grant, con_new_sel} == 2'b10)) begin
+                end else if (irq && ({Bus_Grant, con_new_sel} == 3'b100)) begin
                     Interrupt  = 1;
                     Channel_en_1 = 1;
                     con_sel = con_new_sel;
                     next_state = IDLE;
-                end else if (irq && ({Bus_Grant, con_new_sel} == 2'b11)) begin
+                end else if (irq && ({Bus_Grant, con_new_sel} == 3'b101)) begin
                     Interrupt  = 1;
                     Channel_en_2 = 1;
                     con_sel = con_new_sel;
